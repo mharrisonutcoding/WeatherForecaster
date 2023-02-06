487f6fffea3159e510673af93b6cf33bcc792558
Mike Harrison <mharrisonutcoding@gmail.com>
2023-02-05 19:32:31 -0600

    fixed a few styling errors and started on js logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+let citySearch = $("#city-search");
+let cityInput = $("#city-input");
+let cityHistory = $("#history");
+let weatherResults = $(".card-weather");
+let searchHistoryList = $("#search-history");
+
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+
+$(citySearch).on('submit', function (e) {
+    e.preventDefault();
+
+    formSubmitHandler();
+
+    $("form")[0].reset();
+})
